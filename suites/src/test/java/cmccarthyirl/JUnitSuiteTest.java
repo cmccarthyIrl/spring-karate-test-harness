dbdd5b6c91489ab59f5463899c875febbd09ed2a
cmccarthyirl <craig.mccarthy@live.ie>
2020-11-02 21:23:10 +0000

    failing suite runner

--- suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
+++ suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
@@ -2,12 +2,8 @@ package cmccarthyirl;
 
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Karate.class)
 @Suite.SuiteClasses({
@@ -15,5 +11,4 @@ import org.springframework.boot.test.context.SpringBootTest;
         DynamicParallelKarateTest.class
 })
 @KarateOptions(tags = "~@ignore")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class JUnitSuiteTest { }
