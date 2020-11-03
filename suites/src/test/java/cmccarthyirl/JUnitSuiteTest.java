7385bba4e3b9bfae1667d016262a8820da8216b6
cmccarthyirl <craig.mccarthy@live.ie>
2020-11-03 13:50:55 +0000

    test travis build

--- suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
+++ suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
@@ -1,11 +1,23 @@
 package cmccarthyirl;
 
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import com.intuit.karate.KarateOptions;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
 
+
+//To test the an existing endpoint using the Karate client un-comment the below runner
+//@RunWith(Karate.class)
+//@Suite.SuiteClasses({
+//        BasicParallelKarateTest.class,
+//        DynamicParallelKarateTest.class
+//})
+
+//To test the local application we need to use JUnitPlatform Runner
 @KarateOptions(tags = "~@ignore")
 @RunWith(JUnitPlatform.class)
 @SelectClasses({
