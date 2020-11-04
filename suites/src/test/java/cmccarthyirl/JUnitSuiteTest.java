4f9068e3f57c12020e5317fb8e7866db05d0f381
cmccarthyirl <craig.mccarthy@live.ie>
2020-11-04 14:58:06 +0000

    test travis suite

--- suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
+++ suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
@@ -1,6 +1,10 @@
 package cmccarthyirl;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -10,23 +14,24 @@ import org.junit.platform.suite.api.IncludeEngines;
 //import org.junit.platform.suite.api.SelectClasses;
 
 
-//To test the an existing endpoint using the Karate client un-comment the below runner
-//The suite class will run in the CI with this implementation but will fail as the springApplication doesn't start
-//see sure-fire in pom for disable <testFailureIgnore>
-@RunWith(Karate.class)
-@Suite.SuiteClasses({
-        BasicParallelKarateTest.class,
-        DynamicParallelKarateTest.class
-})
-@IncludeEngines({"junit-jupiter"})
-
-//To test the local application we need to use JUnitPlatform Runner
-//The suite class wont run in the CI with this implementation
-//@KarateOptions(tags = "~@ignore")
-//@RunWith(JUnitPlatform.class)
-//@SelectClasses({
+////To test the an existing endpoint using the Karate client un-comment the below runner
+////The suite class will run in the CI with this implementation but will fail as the springApplication doesn't start
+////see sure-fire in pom for disable <testFailureIgnore>
+//@RunWith(Karate.class)
+//@Suite.SuiteClasses({
 //        BasicParallelKarateTest.class,
 //        DynamicParallelKarateTest.class
 //})
 //@IncludeEngines({"junit-jupiter"})
+
+//To test the local application we need to use JUnitPlatform Runner
+//The suite class wont run in the CI with this implementation
+@KarateOptions(tags = "~@ignore")
+@RunWith(JUnitPlatform.class)
+//@SelectClasses({
+//        BasicParallelKarateTest.class,
+//        DynamicParallelKarateTest.class
+//})
+@SelectPackages({"cmccarthyirl.basic","cmccarthyirl.dynamic"})
+@IncludeEngines({"junit-jupiter"})
 public class JUnitSuiteTest {}
