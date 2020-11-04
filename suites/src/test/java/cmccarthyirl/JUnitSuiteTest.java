7f12ba148c1399002b6404a7284fbf9e2572b19e
cmccarthyirl <craig.mccarthy@live.ie>
2020-11-04 20:46:39 +0000

    test ravis - surefire defined test

--- suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
+++ suites/src/test/java/cmccarthyirl/JUnitSuiteTest.java
@@ -13,32 +13,10 @@ import org.junit.platform.suite.api.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//import com.intuit.karate.KarateOptions;
-//import org.junit.platform.runner.JUnitPlatform;
-
-//import org.junit.platform.suite.api.SelectClasses;
-
-
-////To test the an existing endpoint using the Karate client un-comment the below runner
-////The suite class will run in the CI with this implementation but will fail as the springApplication doesn't start
-////see sure-fire in pom for disable <testFailureIgnore>
-//@ExtendWith(JUnitPlatform.class)
-//@Suite.SuiteClasses({
-//        BasicParallelKarateTest.class,
-//        DynamicParallelKarateTest.class
-//})
-//@IncludeEngines({"junit-jupiter"})
-
-//To test the local application we need to use JUnitPlatform Runner
-//The suite class wont run in the CI with this implementation
 @KarateOptions(tags = "~@ignore")
 @RunWith(JUnitPlatform.class)
-//@SelectClasses({
-//        BasicParallelKarateTest.class,
-//        DynamicParallelKarateTest.class
-//})
 @SuiteDisplayName("JUnit Platform Suite Demo")
-@SelectPackages({"cmccarthyirl.basic","cmccarthyirl.dynamic"})
+//@SelectPackages({"cmccarthyirl.basic","cmccarthyirl.dynamic"})
 //@IncludeEngines({"junit-jupiter"})
 //@ExcludeEngines({"junit-vintage"})
 //@IncludeEngines({"junit-vintage"})
