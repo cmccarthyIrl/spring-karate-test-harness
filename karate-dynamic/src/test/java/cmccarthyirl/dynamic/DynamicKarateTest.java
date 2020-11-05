9477de5f1fb94ebfa402b093ddba190aa875009d
cmccarthyirl <craig.mccarthy@live.ie>
2020-11-05 00:00:54 +0000

    test suite class

--- karate-dynamic/src/test/java/cmccarthyirl/dynamic/DynamicKarateTest.java
+++ karate-dynamic/src/test/java/cmccarthyirl/dynamic/DynamicKarateTest.java
@@ -6,15 +6,19 @@ import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class DynamicKarateTest extends AbstractTestDefinition {
 
 //    @Test
 //    public void DynamicKarateTestAll() {
-//        Results results = Runner.path("classpath:cmccarthyirl/dynamic/ReUseFeaturesTests.feature").parallel(1);
+//        Results results = Runner.path("classpath:c").parallel(1);
 //    }
 
     @Karate.Test
-    Karate dynamicKarateTestAll() {
-        return Karate.run("classpath:cmccarthyirl/dynamic/ReUseFeaturesTests.feature").relativeTo(getClass());
+    Karate dynamicKarateTestAll() throws IOException {
+//        return Karate.run("classpath:cmccarthyirl/dynamic/ReUseFeaturesTests.feature").relativeTo(getClass());
+        return Karate.run(Objects.requireNonNull(getClass().getClassLoader().getResource("cmccarthyirl/dynamic/ReUseFeaturesTests.feature")).getPath()).relativeTo(getClass());
     }
 }
