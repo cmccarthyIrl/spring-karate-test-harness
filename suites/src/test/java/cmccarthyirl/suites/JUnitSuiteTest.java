2e7669f514d5d1a3c14c5c5cc3705b473b53d986
Craig Mc Carthy <craig.mccrthy@live.ie>
2024-08-30 20:02:36 +1000

    Spring 3.3 / Karate 1.5 / JDK 17

--- suites/src/test/java/cmccarthyirl/suites/JUnitSuiteTest.java
+++ suites/src/test/java/cmccarthyirl/suites/JUnitSuiteTest.java
@@ -3,12 +3,11 @@ package cmccarthyirl.suites;
 import cmccarthyirl.basic.BasicParallelKarateTest;
 import cmccarthyirl.basic.config.AbstractTestDefinition;
 import cmccarthyirl.dynamic.DynamicParallelKarateTest;
-import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.ExcludeEngines;
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(JUnitPlatform.class)
+@Suite
 @SelectClasses({DynamicParallelKarateTest.class, BasicParallelKarateTest.class})
 @ExcludeEngines("junit-vintage")
 public class JUnitSuiteTest extends AbstractTestDefinition {
